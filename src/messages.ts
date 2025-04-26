e927b9006c827573949a1af166cee3e1e0f06359
Kip <kip.riecken@gmail.com>
2025-04-26 17:06:43 +0100

    logs to check file read

--- src/messages.ts
+++ src/messages.ts
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 import { sendMessage, getEvent } from "./matrixClientRequests";
 import { PERSON_NAME, ROLE_NAME, PSEUDO_STATE_EVENT_TYPE } from "./constants";
 import { getPseudoState, setPseudoState } from "./pseudoState";
+import * as fs from "fs";
+import * as path from "path";
 
 const { userId } = process.env;
 
@@ -95,6 +97,11 @@ const handleMessage = async (event) => {
   //if message has the tool's wake word, say hello
   if (message === "rory-example") {
     hello(room_id);
+    console.log("dirname is ", __dirname);
+    const filePath = path.join(__dirname.split("dist")[0], "group_ids.json");
+    const jsonData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(jsonData);
+    console.log({ data });
     return;
   }
 };
